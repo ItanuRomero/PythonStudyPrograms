f3c0ec42b8ea9049c45e08fa4affe57f31264f99
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-07-24 16:09:49 -0300

    Create ExampleDefFcunctions.py

---
+++ ProgramsToRead/ExampleDefFcunctions.py
@@ -0,0 +1,17 @@
+'''
+Exercício Python 096: Faça um programa que tenha uma função chamada área(), que receba as dimensões de um terreno retangular (largura e comprimento) e mostre a área do terreno.
+'''
+def area(lateral1, lateral2):
+    area_terreno = lateral1 * lateral2
+    print(f'O terreno possui a area:\n'
+          f'{lateral1:.2f}m X {lateral2:.2f}m, igual a -> {area_terreno:.2f}m2')
+
+
+def titulo_programa(escrever):
+    print(f'{escrever:-^40}')
+
+
+titulo_programa(' CONTROLE DE TERRENOS ')
+largura = float(input('Largura em metros: '))
+comprimento = float(input('Comprimento em metros: '))
+area(largura, comprimento)
