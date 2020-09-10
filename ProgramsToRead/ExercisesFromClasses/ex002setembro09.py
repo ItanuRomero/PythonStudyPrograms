a73d4b960508c9ae06851e8aa9ed977ba57240cb
ItanuRomero <66224956+ItanuRomero@users.noreply.github.com>
2020-09-09 21:04:00 -0300

    Update ex002setembro09.py

--- ProgramsToRead/ExercisesFromClasses/ex002setembro09.py
+++ ProgramsToRead/ExercisesFromClasses/ex002setembro09.py
@@ -1,23 +1,13 @@
-arquivo = open('dado.txt', 'w+')
-while True:
-    if arquivo.read() == '':
-        print('Arquivo esta vazio')
-        aluno = [str(input('Nome do aluno: ')), str(input('Nota 1: ')), str(input('Nota 2: '))
-        ]
-        for dados in aluno:
-            arquivo.write(dados)
-            arquivo.write('\n')
-        arquivo.close()
-    else:
-        nome = arquivo.readline()
-        nota1 = int(arquivo.readline())
-        nota2 = int(arquivo.readline())
-        media = (nota1 + nota2) / 2
-        print(f'Nome do aluno: {nome}\n'
-              f'Nota 1: {nota1}\n'
-              f'Nota 2: {nota2}\n'
-              f'Media: {media}')
-        arquivo.close()
-    resposta = str(input('Abrir novamente? [s/n]')).strip().lower()[0]
-    if resposta == 'n':
-        break
+arquivo = open('dado.txt', 'r')
+
+nome = arquivo.readline().strip()
+nota1 = arquivo.readline().strip()
+nota2 = arquivo.readline().strip()
+nota1 = int(nota1)
+nota2 = int(nota2)
+media = (nota1 + nota2) / 2
+print(f'Nome do aluno: {nome}\n'
+      f'Nota 1: {nota1}\n'
+      f'Nota 2: {nota2}\n'
+      f'Media: {media}')
+arquivo.close()
